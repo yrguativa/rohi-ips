5c26669b0d600c3b2dc3bea27e13cec8df4458aa
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-09-26 13:47:40 -0500

    deploy v0.0.5 remote HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { store } from './store'
 import App from './App.tsx'
@@ -11,9 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <HashRouter basename="/">
         <App />
-      </HashRouter>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,

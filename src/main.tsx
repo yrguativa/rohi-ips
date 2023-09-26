c42d52582fe5fe7840d87e8dc8ea17a2fdc9a1dd
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-09-26 17:08:27 -0500

    fix router /ResponsePayment test

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/rohi-ips/'}>
         <App />
       </BrowserRouter>
     </Provider>

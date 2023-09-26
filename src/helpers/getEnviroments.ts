5e1993fbc7ebfc90e3b622e2e2b045bd2f8ffe21
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-09-26 15:44:08 -0500

    fix error with getEnvironments function

--- src/helpers/getEnviroments.ts
+++ src/helpers/getEnviroments.ts
@@ -1,7 +1,16 @@
 export const getEnvironments = () => {
-    import.meta.env;
+
+    const firebaseConfig = {
+        VITE_FBASE_APIKEY: import.meta.env.VITE_FBASE_APIKEY,
+        VITE_FBASE_AUTHDOMAIN: import.meta.env.VITE_FBASE_AUTHDOMAIN,
+        VITE_FBASE_PROJECTID: import.meta.env.VITE_FBASE_PROJECTID,
+        VITE_FBASE_STORAGEBUCKET: import.meta.env.VITE_FBASE_STORAGEBUCKET,
+        VITE_FBASE_MESSAGINGSENDERID: import.meta.env.VITE_FBASE_MESSAGINGSENDERID,
+        VITE_FBASE_APPID: import.meta.env.VITE_FBASE_APPID,
+        VITE_FBASE_DATABASEURL: import.meta.env.VITE_FBASE_DATABASEURL,
+    };
 
     return {
-        ...import.meta.env
+        ...firebaseConfig
     }
 }

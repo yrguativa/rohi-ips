5217760ed883ad50bed2a36db4caba39cd02576d
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-09-26 13:22:09 -0500

    deploy v0.0.2 change url base for hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import { store } from './store'
 import App from './App.tsx'
@@ -11,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      <HashRouter basename="/">
         <App />
+      </HashRouter>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,

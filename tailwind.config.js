f940407e4eb18f4328ec72b2691c6011c32602d7
Yilmer Ramiro Guativa Rincon <yilmer.guativa@siigo.com>
2024-04-09 17:39:31 -0500

    ♻️ restore configuration by tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: 'class',
     theme: {
